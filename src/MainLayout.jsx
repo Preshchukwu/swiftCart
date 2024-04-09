119ed2deffe1d6fbafa6a39148b6b60188d2db45
Precious_chukwu <preciousnwachukwu891@gmail.com>
2024-04-09 02:38:29 +0100

    update on carousel

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -10,6 +10,17 @@ const MainLayout = () => {
     <div className='main'>
       <Nav> </Nav>
 
+        
+
+        <div className="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item">Try pick one or two</div>
+                <div className="carousel-item">Try pick one or two</div>
+                <div className="carousel-item">Try pick one or two</div>
+            </div>
+        </div>
+
+
         <div className='theMain'>
             <Effect></Effect>
         </div>
